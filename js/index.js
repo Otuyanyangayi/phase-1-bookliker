efd7fc755f37968048c386c4412c894c03e0a56d
Grace <gracenyangayi@gmail.com>
2022-06-17 08:16:21 +0300

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    async function getBooks() {
+         fetch('http://localhost:3000/books');
+        return await resp.json();
+        }
+        function createBookLI(bookObject) {
+            const liElement = document.createElement('li');
+            liElement.textContent = bookObject.title;
+            liElement.addEventListener("click",showBookDetails)
+            return liElement;
+            }
+        
+        function showBooksList(booksArray) {
+            const LIContainer = document.getElementById('list');
+            booksArray.forEach(bookObject => {
+                const bookLI = createBookLI(bookObject);
+                LIContainer.appendChild(bookLI);
+                });
+            };
+        
+        function showBookDetails(event) {
+            alert(event.target.innerText);
+            event.preventDefault()
+            }
+        
+        getBooks().then(showBooksList);
+
+});
